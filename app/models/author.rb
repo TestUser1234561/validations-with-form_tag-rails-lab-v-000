693d35958168ce04888cb3fcb7dff3ddd239041a
Morgan Fudge <mfudge92@gmail.com>
2018-04-11 19:44:28 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,5 +2,5 @@ class Author < ActiveRecord::Base
 validates :name, presence: true
   validates :email, uniqueness: true
   validates(:content, { :length => { :minimum => 100 } })
-  validates :category , inclusion: { in: %w(Fiction Non-Fiction),
+  validates :category , inclusion: { in: %w(Fiction Non-Fiction) }
 end
