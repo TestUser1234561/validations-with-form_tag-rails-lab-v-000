a4ecfa970cb39bcb4d89a6bffd8ae4184700c71f
Morgan Fudge <mfudge92@gmail.com>
2018-04-11 19:51:52 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,16 +10,11 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
-
-
-    if @author.save
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
     end
-
-    redirect_to post_path(@post)
   end
 
   private
